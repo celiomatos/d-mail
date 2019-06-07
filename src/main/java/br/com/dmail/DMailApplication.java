39165b61251c5c2fa8ac725b1d73a3afb85f3801
Celio Matos <celio.matos@itbam.org.br>
2019-06-07 15:54:02 -0400

    Initial commit

---
+++ src/main/java/br/com/dmail/DMailApplication.java
@@ -0,0 +1,13 @@
+package br.com.dmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DMailApplication.class, args);
+	}
+
+}
